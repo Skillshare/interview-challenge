52c1af4ac4ec51fd11f7ff07662c9c2ef3087c16
Joseph Michael Pasque <joseph.pasque@skillshare.com>
2025-02-07 14:27:09 -0600

    fix: type skeleton for questions

---
+++ ts/src/some-types.d.ts
@@ -0,0 +1,12 @@
+/**
+ * NOTE: This file is not related to the coding challenge
+ */
+
+export type BaseClass = {
+    title: string;
+    description: string;
+}
+
+export type Likeable = {
+    likeCount: number;
+}
